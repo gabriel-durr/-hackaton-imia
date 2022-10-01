b8d37c5736b20e342bb87c12fc7f0f4b7c15e054
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-01 12:00:28 -0300

    :tada: add chakra + nextjs

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+/* document google fonts and chakra color mode config */
+
+import Document, {Head, Html, Main, NextScript} from "next/document";
+
+export default class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+						rel="stylesheet"
+					/>
+
+					<body>
+						<Main />
+						<NextScript />
+					</body>
+				</Head>
+			</Html>
+		);
+	}
+}
---
+++ src/styles/global.js
@@ -0,0 +1,64 @@
+import {extendTheme} from "@chakra-ui/react";
+
+const styles = {
+	global: {
+		"html, body": {
+			bg: "gray.400",
+			color: "grey.900",
+			fontSize: "0.95rem",
+			overflowX: "hidden",
+			scrollBehavior: "smooth",
+			scrollPaddingTop: "120px",
+			"&::-webkit-scrollbar": {
+				width: "8px",
+				backgroundColor: "whiteAlpha.900",
+			},
+			"&::-webkit-scrollbar-thumb": {
+				width: "8px",
+				backgroundColor: "gray.600",
+				borderRadius: "4px",
+			},
+		},
+
+		a: {
+			color: "grey.900",
+			_hover: {
+				textDecoration: "none !important",
+				outline: "none",
+				border: "none",
+			},
+		},
+		"*::placeholder": {
+			color: "gray.500",
+		},
+	},
+};
+
+export const colors = {
+	grey: {
+		900: "#181B23",
+		800: "#212529",
+		700: "#343A40",
+		600: "#495057",
+		500: "#6C757D",
+		400: "#ADB5BD",
+		300: "#CED4DA",
+		200: "#DEE2E6",
+		100: "#E9ECEF",
+		50: "#dad9d6",
+	},
+};
+
+const fonts = {
+	heading: "Rubik, sans-serif",
+	body: "Open Sans, sans-serif",
+};
+
+const theme = extendTheme({
+	styles,
+	colors,
+	fonts,
+	components: {},
+});
+
+export default theme;
