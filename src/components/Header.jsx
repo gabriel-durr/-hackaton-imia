91388dd990838c785007f5847c2bbf9d40c7ba7d
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-01 14:45:32 -0300

    att

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,25 @@ export const Header = () => {
 			h="9rem"
 			w="100vw"
 			align="center"
-			px="3rem"
+			px="2rem"
 			justify="space-between"
 			bgGradient="linear(90deg, #0a5779 0%,  #1976a2 100%, #21889b 100%)">
-			<Image alt="iMia" src="/i-mia.png" w="280px" h="120px" />
+			<Image alt="iMia" src="/i-mia.png" w="290px" h="120px" />
 			<Button
 				rounded="full"
 				variant="outline"
-				p="1.5rem"
+				p="1.6rem"
 				_hover={{
 					bg: "#5e8a9c56",
 					transition: ".4s ease",
 				}}>
-				<MdOutlineAccountCircle size="1.7rem" color="#fff" />
-				<Text as="span" color="#fff" ml="5px">
+				<MdOutlineAccountCircle size="1.8rem" color="#fff" />
+				<Text
+					fontSize="1.09rem"
+					as="span"
+					color="#fff"
+					ml="5px"
+					fontWeight="thin">
 					Dr. Ingo Hoffman
 				</Text>
 			</Button>
