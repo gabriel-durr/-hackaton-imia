af2ef7d0edb9a3ab33a730d68dd03c15c4e2fd78
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-06 02:01:17 -0300

    :lipstick: update drag in drop and styles

--- src/components/templates/tab1/Forms.jsx
+++ src/components/templates/tab1/Forms.jsx
@@ -1,19 +1,73 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {Flex, Button, Text, Input} from "@chakra-ui/react";
+import {Dropzone} from "../../../utils/Dropzone";
 import {DraggableList} from "../../../utils/DraggableList";
-import {mock} from "../../../utils/mock";
 
-export const Forms = ({}) => {
+export const Forms = ({data}) => {
+	const [title, setTitle] = useState("");
+	const [hasFile, setHasFile] = useState(false);
+	const [keysList, setKeysList] = useState([]);
+
+	const onHandleKeys = async () => {
+		if (hasFile) {
+			setKeysList(data);
+		}
+	};
+
 	return (
 		<Flex
-			w="100%"
-			h="100%"
-			align="center"
+			align="flex-start"
 			px="2rem"
-			justify="flex-start"
-			overflow="hidden"
+			justify="center"
+			margin="15px"
 			direction="column">
-			<DraggableList data={mock} />
+			<Text
+				fontSize="1.5rem"
+				fontWeight="bold"
+				as="span"
+				color="#000"
+				ml="5px">
+				Titulo
+			</Text>
+			<Input
+				type="text"
+				border="1px #000 solid"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+			/>
+			<Flex direction="row" align="center" py="1em">
+				<Dropzone onHandleDrag={setHasFile} />
+				{hasFile && (
+					<Button
+						variant="outline"
+						flexDirection="column"
+						width="15em"
+						height="5em"
+						bg="#0a5779"
+						marginLeft="10px"
+						_hover={{
+							bg: "#5e8a9c56",
+							transition: ".4s ease",
+						}}
+						onClick={() => onHandleKeys()}>
+						<Text
+							fontSize="1.09rem"
+							as="span"
+							color="#fff"
+							ml="5px"
+							fontWeight="thin">
+							Import document
+						</Text>
+					</Button>
+				)}
+			</Flex>
+			<Flex
+				direction="row"
+				justifyContent="space-between"
+				w="40em"
+				alignItems="flex-end">
+				<DraggableList list={keysList} setItemList={setKeysList} />
+			</Flex>
 		</Flex>
 	);
 };
--- src/utils/mock.js
+++ src/utils/mock.js
@@ -3,15 +3,17 @@ export const mock = [
 		id: "1",
 		label: "Produtos",
 		items: [
-			{id: "2", label: "Item 1"},
-			{id: "3", label: "Item 2"},
+			{id: "2", label: "Mercado"},
+			{id: "3", label: "Material"},
+			{id: "5", label: "Ano/Mês"},
+			{id: "6", label: "Item 2"},
 		],
 	},
 	{
 		id: "4",
 		label: "Vendas",
 		items: [
-			{id: "5", label: "Item 1"},
+			{id: "5", label: "Ano/Mês"},
 			{id: "6", label: "Item 2"},
 		],
 	},
@@ -19,8 +21,8 @@ export const mock = [
 		id: "7",
 		label: "Casa",
 		items: [
-			{id: "8", label: "Item 1"},
-			{id: "9", label: "Item 2"},
+			{id: "8", label: "Mercado Interno"},
+			{id: "9", label: "Mercado Externo"},
 		],
 	},
 ];
