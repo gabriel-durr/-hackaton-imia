9bc632c46e0967b2115c50c8df6916dce024c206
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-07 19:26:24 -0300

    ...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -58,7 +58,7 @@ export default function Home({data}) {
 }
 
 export async function getServerSideProps() {
-	// Fetch data from external API
+	// Fetch data from external API.
 	let data = await axios.get(process.env.URL_API).then(res => res.data);
 
 	// Pass data to the page via props
