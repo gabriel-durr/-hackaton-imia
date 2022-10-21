1c78d0bd56cd90b8f0a4cf4148d1001a94c80ae5
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-20 23:24:08 -0300

    ..

--- src/components/templates/tab3/Graph.jsx
+++ src/components/templates/tab3/Graph.jsx
@@ -14,21 +14,8 @@ const Plot = dynamic(() => import("react-plotly.js"), {
 	ssr: false,
 });
 
-import {
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
-	ModalBody,
-	ModalCloseButton,
-	useDisclosure,
-	Text,
-	Textarea,
-	Spinner,
-	Button,
-	Flex,
-} from "@chakra-ui/react";
+import {Spinner, useDisclosure} from "@chakra-ui/react";
+import {ModalGraph} from "./ModalGraph";
 
 export const Graph = ({data}) => {
 	const {isOpen, onOpen, onClose} = useDisclosure();
@@ -165,7 +152,6 @@ export const Graph = ({data}) => {
 						clearTimeout(timer);
 						timer = setTimeout(() => {
 							console.log("opa");
-							setCanHover(true);
 							onHandleModal(lastEvent);
 						}, 500);
 					}
@@ -176,59 +162,8 @@ export const Graph = ({data}) => {
 					}
 				}}
 			/>
-			<Modal isOpen={isOpen} onClose={onClose}>
-				{console.log("dentro do modal")}
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Dados do evento</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						{!!event &&
-							event.map((e, i) => {
-								return i === 0 ? (
-									<Flex
-										key={i}
-										direction="row"
-										justify="center"
-										align="center">
-										<Text fontWeight="bold">{e}</Text>
-									</Flex>
-								) : (
-									<Flex
-										key={i}
-										display="flex"
-										flexDirection="row"
-										justifyContent="space-between"
-										align="center">
-										<Text>{e}</Text>
-										<Button
-											colorScheme="blue"
-											marginTop={5}
-											mr={3}
-											onClick={onClose}>
-											Abrir Coleção
-										</Button>
-									</Flex>
-								);
-							})}
-						<Text marginTop="10px" fontWeight="bold">
-							Observações:
-						</Text>
-						<Textarea
-							size="sm"
-							placeholder="Coloque aqui suas observações ..."
-						/>
-					</ModalBody>
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={onClose}>
-							Send
-						</Button>
-						<Button colorScheme="red" mr={3} onClick={onClose}>
-							Close
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
+
+			<ModalGraph event={event} isOpen={isOpen} onCLose={onClose} />
 		</>
 	);
 };
---
+++ src/components/templates/tab3/ModalGraph.jsx
@@ -0,0 +1,72 @@
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+	Text,
+	Button,
+	Textarea,
+	Flex,
+} from "@chakra-ui/react";
+
+export const ModalGraph = ({event, isOpen, onClose}) => {
+	return (
+		<Modal isOpen={isOpen} onClose={onClose}>
+			{console.log("dentro do modal")}
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Dados do evento</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					{event &&
+						event.map((e, i) => {
+							return i === 0 ? (
+								<Flex
+									key={i}
+									direction="row"
+									justify="center"
+									align="center">
+									<Text fontWeight="bold">{e}</Text>
+								</Flex>
+							) : (
+								<Flex
+									key={i}
+									display="flex"
+									flexDirection="row"
+									justifyContent="space-between"
+									align="center">
+									<Text>{e}</Text>
+									<Button
+										colorScheme="blue"
+										marginTop={5}
+										mr={3}
+										onClick={() => onClose}>
+										Abrir Coleção
+									</Button>
+								</Flex>
+							);
+						})}
+					<Text marginTop="10px" fontWeight="bold">
+						Observações:
+					</Text>
+					<Textarea
+						size="sm"
+						placeholder="Coloque aqui suas observações ..."
+					/>
+				</ModalBody>
+				<ModalFooter>
+					<Button colorScheme="blue" mr={3} onClick={() => onClose}>
+						Send
+					</Button>
+					<Button colorScheme="red" mr={3} onClick={() => onClose}>
+						Close
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	);
+};
